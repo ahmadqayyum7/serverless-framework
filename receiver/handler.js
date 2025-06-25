0319191c0202773415c1cf34170d47c86f16dc4d
ahmadqayyum7 <ahmadqayyum19@gmail.com>
2025-06-25 18:59:35 +0500

    checking logs

--- receiver/handler.js
+++ receiver/handler.js
@@ -3,17 +3,31 @@ const client = new SQSClient();
 const QUEUE_URL = process.env.QUEUE_URL;
 
 module.exports.handler = async (event) => {
-  const body = JSON.parse(event.body);
+  try {
+    console.log("Received event:", event);
 
-  const command = new SendMessageCommand({
-    QueueUrl: QUEUE_URL,
-    MessageBody: JSON.stringify(body)
-  });
+    const body = JSON.parse(event.body);
+    console.log("Parsed body:", body);
 
-  await client.send(command);
+    const command = new SendMessageCommand({
+      QueueUrl: QUEUE_URL,
+      MessageBody: JSON.stringify(body)
+    });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Message sent to SQS" })
-  };
+    console.log("Sending message to SQS...");
+    const response = await client.send(command);
+    console.log("Message sent. SQS response:", response);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Message sent to SQS" })
+    };
+
+  } catch (err) {
+    console.error("Error in receiver Lambda:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal server error", details: err.message })
+    };
+  }
 };
